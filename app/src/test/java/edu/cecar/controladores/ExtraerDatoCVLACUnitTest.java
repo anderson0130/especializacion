d363984e9cbe5f86e6f666757564c8698040e3d1
1102845116 <anderson.perez@cecar.edu.co>
2019-03-09 09:32:17 -0500

    Implementación del test para el metodo extraerDatosCVLAC

--- app/src/test/java/edu/cecar/controladores/ExtraerDatoCVLACUnitTest.java
+++ app/src/test/java/edu/cecar/controladores/ExtraerDatoCVLACUnitTest.java
@@ -1,5 +1,6 @@
 package edu.cecar.controladores;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExtraerDatoCVLACUnitTest {
+    @Rule
+    public GestionTestExtraerDatoCVLAC gestionTestExtraerDatoCVLAC = new GestionTestExtraerDatoCVLAC();
     @Test
-    public void testDatosCVLAC() {
-     
+    public void validarDatosCVLAC() {
+
+        // se definen los datos de referencia con los que se probara el metodo extraerDatos
+
+        Investigador investigador1 = ExtraerDatoCVLAC.getDatos("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0001376707");
+        Investigador investigador2 = ExtraerDatoCVLAC.getDatos("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000787132");
+        // se define los datos esperados
+        String nombreInvestigador1 = "Guillermo Carlos Hernández Hernández";
+        String nombreInvestigador2 = "Namuel Francisco Solórzano Peralta";
+
+        // se comprueba o testea el valor esperado con el obtenido
+
+        assertEquals(nombreInvestigador1,investigador1.getNombres());
+        assertEquals(nombreInvestigador2,investigador2.getNombres());
     }
-}
+     @Test
+      public void testDatosCVLACUno () {
+         // se obtienen los dartos del investigador.
+         Investigador investigador = ExtraerDatoCVLAC.getDatos("http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000402478");
+
+         //se define los datos esperados.
+         String nombreInvestigador = "Luty Del Carmen Gomezcaceres Peréz";
+         assertEquals(nombreInvestigador, investigador.getNombres());
+     }
+
+     }
+
+
